c3e3e86c46a7df4f3a7a27d6cf525f0006ef2598
Christian Lee <christian.lee1398@gmail.com>
2023-07-12 13:35:30 -0400

    loop API object

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-// import axios from 'axios';
-axios.get('https://nba-stats-db.herokuapp.com/api/playerdata/name/Lebron James')
-    .then(function (response) {
-        // handle success
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
+const form = document.querySelector('#searchForm')
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const userInput = form.elements.query.value;
+    console.log(userInput);
+    axios.get(`https://nba-stats-db.herokuapp.com/api/playerdata/name/${userInput}`)
+        .then(function (res) {
+            // handle success
+            const stats = res.data.results[0];
+            console.log(stats)
+            const statKeys = Object.keys(stats)
+            const statValues = Object.values(stats)
+
+            const statObj = {
+                points: `${statValues[27]}`,
+                assists: `${statValues[22]}`,
+                rebounds: `${statValues[21]}`,
+                threePct: `${statValues[11]}`,
+                fgPct: `${statValues[15]}`,
+                steals: `${statValues[23]}`,
+            }
+
+            const entries = Object.entries(statObj)
+            console.log(entries)
+            const statList = document.getElementById('#result');
+            for (i = 0; i < entries.length; ++i) {
+                let li = document.createElement('li');
+                li.innerText = entries[i];
+                statList.appendChild(li);
+            }
+
+        })
+        .catch(function (err) {
+            // handle error
+            console.log(err);
+        })
+})
+
+// const displayStats = (stats) => {
+//     for (let stat of stats) {
+//         console.log(stat)
+//     }
+// }
