67660c02d9c948278799ac1653c046902743360d
Christian Lee <christian.lee1398@gmail.com>
2023-07-16 15:26:09 -0400

    add second event listener

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const form = document.querySelector('#searchForm')
-form.addEventListener('submit', function (e) {
+const form1 = document.querySelector('#searchForm1')
+const form2 = document.querySelector('#searchForm2')
+form1.addEventListener('submit', function (e) {
     e.preventDefault();
-    const userInput = form.elements.query.value;
+    const userInput = form1.elements.query.value;
     console.log(userInput);
     axios.get(`https://nba-stats-db.herokuapp.com/api/playerdata/name/${userInput}`)
         .then(function (res) {
@@ -11,22 +12,60 @@ form.addEventListener('submit', function (e) {
             const statKeys = Object.keys(stats)
             const statValues = Object.values(stats)
 
-            const statObj = {
-                'Points': `${statValues[27]}`,
-                'Assists': `${statValues[22]}`,
-                'Rebounds': `${statValues[21]}`,
-                '3-point %': `${statValues[11]}`,
-                'FG %': `${statValues[15]}`,
-                'Steals': `${statValues[23]}`,
+            const statArr = [
+                `Points: ${statValues[27]}`,
+                `Assists: ${statValues[22]}`,
+                `Rebounds: ${statValues[21]}`,
+                `3-Point %: ${statValues[11]}`,
+                `FG %: ${statValues[15]}`,
+                `Steals: ${statValues[23]}`
+            ]
+
+            const statList = document.querySelector('#result1');
+            for (i = 0; i < statArr.length; i++) {
+                let li = document.createElement('li');
+                li.innerText = statArr[i];
+                statList.appendChild(li);
+                if (statList.length) {
+                    statList.parentNode.removeChild(li)
+                }
             }
 
-            const entries = Object.entries(statObj)
-            console.log(entries)
-            const statList = document.querySelector('#result');
-            for (i = 0; i < entries.length; i++) {
+        })
+        .catch(function (err) {
+            // handle error
+            console.log(err);
+        })
+})
+form2.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const userInput = form2.elements.query.value;
+    console.log(userInput);
+    axios.get(`https://nba-stats-db.herokuapp.com/api/playerdata/name/${userInput}`)
+        .then(function (res) {
+            // handle success
+            const stats = res.data.results[0];
+            console.log(stats)
+            const statKeys = Object.keys(stats)
+            const statValues = Object.values(stats)
+
+            const statArr = [
+                `Points: ${statValues[27]}`,
+                `Assists: ${statValues[22]}`,
+                `Rebounds: ${statValues[21]}`,
+                `3-Point %: ${statValues[11]}`,
+                `FG %: ${statValues[15]}`,
+                `Steals: ${statValues[23]}`
+            ]
+
+            const statList = document.querySelector('#result2');
+            for (i = 0; i < statArr.length; i++) {
                 let li = document.createElement('li');
-                li.innerText = entries[i];
+                li.innerText = statArr[i];
                 statList.appendChild(li);
+                if (statList.length) {
+                    statList.parentNode.removeChild(li)
+                }
             }
 
         })
