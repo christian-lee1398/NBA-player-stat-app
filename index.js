75be0f9ca415c1e1f2a70ca8510623a1472af1d8
Christian Lee <christian.lee1398@gmail.com>
2023-07-17 14:29:51 -0400

    disable button after submission

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const form1 = document.querySelector('#searchForm1')
 const form2 = document.querySelector('#searchForm2')
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('#btn2')
+
 form1.addEventListener('submit', function (e) {
     e.preventDefault();
+    btn1.disabled = true;
     const userInput = form1.elements.query.value;
     console.log(userInput);
     axios.get(`https://nba-stats-db.herokuapp.com/api/playerdata/name/${userInput}`)
         .then(function (res) {
             // handle success
             const stats = res.data.results[0];
-            console.log(stats)
-            const statKeys = Object.keys(stats)
             const statValues = Object.values(stats)
 
             const statArr = [
@@ -26,8 +28,8 @@ form1.addEventListener('submit', function (e) {
                 let li = document.createElement('li');
                 li.innerText = statArr[i];
                 statList.appendChild(li);
-                if (statList.length) {
-                    statList.parentNode.removeChild(li)
+                if (statList.length > 6) {
+                    statList.innerHTML = '';
                 }
             }
 
@@ -39,14 +41,13 @@ form1.addEventListener('submit', function (e) {
 })
 form2.addEventListener('submit', function (e) {
     e.preventDefault();
+    btn2.disabled = true;
     const userInput = form2.elements.query.value;
     console.log(userInput);
     axios.get(`https://nba-stats-db.herokuapp.com/api/playerdata/name/${userInput}`)
         .then(function (res) {
             // handle success
             const stats = res.data.results[0];
-            console.log(stats)
-            const statKeys = Object.keys(stats)
             const statValues = Object.values(stats)
 
             const statArr = [
